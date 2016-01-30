749a2b5f333ffc2650410d6fa221a9e9a2cd1fe0
Miralem Cebic <mail@miralem-cebic.de>
2016-01-30 17:08:45 +0100

    detail view and buy-per-paypall

--- app/templates/templates.js
+++ app/templates/templates.js
@@ -8,11 +8,27 @@ angular.module('templateStore.templates', ['ngRoute'])
         when('/templates', {
             templateUrl:  'templates/templates.html',
             controller: 'TemplatesCtrl'
+        }).
+        when('/templates/:templateId', {
+            templateUrl:  'templates/template-detail.html',
+            controller: 'TemplatesDetailsCtrl'
+        })
+}])
+
+.controller('TemplatesCtrl', ['$scope', '$http', function($scope, $http) {
+
+    $http.get('json/templates.json').success(function(data) {
+        $scope.templates = data;
     });
 }])
 
-.controller('TemplatesCtrl', ['$scope', function($scope) {
+.controller('TemplatesDetailsCtrl', ['$scope', '$http', '$routeParams', '$filter', function($scope, $http, $routeParams, $filter) {
 
-    console.log("Templates working");
-    console.log($scope);
+    var templateId = $routeParams.templateId;
+    $http.get('json/templates.json').success(function(data) {
+        $scope.template = $filter('filter')(data, function(d) {
+            return d.id == templateId;
+        })[0];
+        $scope.mainImage = $scope.template.images[0].name;
+    });
 }]);
